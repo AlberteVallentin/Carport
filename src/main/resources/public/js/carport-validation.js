5e47b36e71645eace5d76a29afe9b5c9f2321d23
AlberteVallentin <143842711+AlberteVallentin@users.noreply.github.com>
2024-05-05 21:07:45 +0200

    Updated validation

--- src/main/resources/public/js/carport-validation.js
+++ src/main/resources/public/js/carport-validation.js
@@ -1,6 +1,8 @@
 const carportWidth = document.getElementById('carport-width');
 const carportLength = document.getElementById('carport-length');
 const carportRoof = document.getElementById('carport-roof');
+const shedWidth = document.getElementById('shed-width');
+const shedLength = document.getElementById('shed-length');
 const validateLink = document.getElementById('validateLink');
 
 // Funktion til at sætte en fejlbesked på et specifikt element
@@ -30,13 +32,27 @@ const validateSelect = (selectElement, errorMessage) => {
     }
 };
 
-// Samlet valideringsfunktion, som kontrollerer alle inputs
 const validateInputs = () => {
     const isValidWidth = validateSelect(carportWidth, 'Vælg venligst en bredde');
     const isValidLength = validateSelect(carportLength, 'Vælg venligst en længde');
     const isValidRoof = validateSelect(carportRoof, 'Vælg venligst en tagtype');
+    const isValidShedWidth = validateSelect(shedWidth, 'Vælg venligst en bredde eller "Uden redskabsrum"');
+    const isValidShedLength = validateSelect(shedLength, 'Vælg venligst en længde eller "Uden redskabsrum"');
 
-    return isValidWidth && isValidLength && isValidRoof;  // Returner 'true' kun hvis alle er gyldige
+    // Tjek om brugeren har valgt "Uden redskabsrum" i længde, men har valgt en bredde (tjekker først om der er valgt en bredde og længde)
+    if (isValidShedWidth && isValidShedLength && shedLength.value === "Uden redskabsrum" && shedWidth.value !== "Uden redskabsrum") {
+        setError(shedLength, 'Vælg "Uden redskabsrum" eller både en længde og bredde for redskabsrummet');
+        setError(shedWidth, 'Vælg "Uden redskabsrum" eller både en længde og bredde for redskabsrummet');
+        return false;
+    }
+    // Tjek om brugeren har valgt "Uden redskabsrum" i bredde, men har valgt en længde (tjekker først om der er valgt en bredde og længde)
+    if (isValidShedWidth && isValidShedLength && shedWidth.value === "Uden redskabsrum" && shedLength.value !== "Uden redskabsrum") {
+        setError(shedWidth, 'Vælg "Uden redskabsrum" eller både en længde og bredde for redskabsrummet');
+        setError(shedLength, 'Vælg "Uden redskabsrum" eller både en længde og bredde for redskabsrummet');
+        return false;
+    }
+
+    return isValidWidth && isValidLength && isValidRoof && isValidShedWidth && isValidShedLength;  // Returner 'true' kun hvis alle er gyldige
 };
 
 // Event listener på linket, der validerer data før navigation
