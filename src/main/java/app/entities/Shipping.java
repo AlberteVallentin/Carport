4233dbb2eced47fa5d7fcb7bb5c3f2a56a252104
AlberteVallentin <143842711+AlberteVallentin@users.noreply.github.com>
2024-05-10 21:43:12 +0200

    more classes

---
+++ src/main/java/app/entities/Shipping.java
@@ -0,0 +1,25 @@
+package app.entities;
+
+public class Shipping {
+    private int shippingId;
+    private int adressId;
+    private double shippingRate;
+
+    public Shipping(int shippingId, int adressId, double shippingRate) {
+        this.shippingId = shippingId;
+        this.adressId = adressId;
+        this.shippingRate = shippingRate;
+    }
+
+    public int getShippingId() {
+        return shippingId;
+    }
+
+    public int getAdressId() {
+        return adressId;
+    }
+
+    public double getShippingRate() {
+        return shippingRate;
+    }
+}
