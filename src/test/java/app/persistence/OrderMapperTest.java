70643ccd39ece613d026f29c6301dd116ecbc765
AlberteVallentin <143842711+AlberteVallentin@users.noreply.github.com>
2024-05-18 11:29:14 +0200

    test

---
+++ src/test/java/app/persistence/OrderMapperTest.java
@@ -0,0 +1,33 @@
+package app.persistence;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderMapperTest {
+    // Integrationstest for OrderMapper
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance();
+
+    @BeforeAll
+    static void setupClass() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void getOrderByOrderId() {
+    }
+
+    @Test
+    void insertOrder() {
+    }
+}
