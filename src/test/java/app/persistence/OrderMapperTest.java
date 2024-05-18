2e7da7ccf8a8b79a03dff8f9ada7a1565434c2a8
AlberteVallentin <143842711+AlberteVallentin@users.noreply.github.com>
2024-05-18 12:27:56 +0200

    Nu med shipping

--- src/test/java/app/persistence/OrderMapperTest.java
+++ src/test/java/app/persistence/OrderMapperTest.java
@@ -1,5 +1,7 @@
 package app.persistence;
 
+import app.entities.Order;
+import app.exceptions.DatabaseException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,16 +25,21 @@ class OrderMapperTest {
                 // The test schema is already created, so we only need to delete/create test tables
                 stmt.execute("DROP TABLE IF EXISTS test.users");
                 stmt.execute("DROP TABLE IF EXISTS test.orders");
+                stmt.execute("DROP TABLE IF EXISTS test.shipping");
                 stmt.execute("DROP SEQUENCE IF EXISTS test.users_user_id_seq CASCADE;");
                 stmt.execute("DROP SEQUENCE IF EXISTS test.orders_order_id_seq CASCADE;");
+                stmt.execute("DROP SEQUENCE IF EXISTS test.shipping_shipping_id_seq CASCADE;"); // Add this line
                 // Create tables as copy of original public schema structure
                 stmt.execute("CREATE TABLE test.users AS (SELECT * from public.users) WITH NO DATA");
                 stmt.execute("CREATE TABLE test.orders AS (SELECT * from public.orders) WITH NO DATA");
-                // Create sequences for auto generating id's for users and orders
+                stmt.execute("CREATE TABLE test.shipping AS (SELECT * from public.shipping) WITH NO DATA");
+                // Create sequences for auto generating id's for users, orders and shipping
                 stmt.execute("CREATE SEQUENCE test.users_user_id_seq");
                 stmt.execute("ALTER TABLE test.users ALTER COLUMN user_id SET DEFAULT nextval('test.users_user_id_seq')");
                 stmt.execute("CREATE SEQUENCE test.orders_order_id_seq");
                 stmt.execute("ALTER TABLE test.orders ALTER COLUMN order_id SET DEFAULT nextval('test.orders_order_id_seq')");
+                stmt.execute("CREATE SEQUENCE test.shipping_shipping_id_seq"); // And this line
+                stmt.execute("ALTER TABLE test.shipping ALTER COLUMN shipping_id SET DEFAULT nextval('test.shipping_shipping_id_seq');");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -46,6 +54,7 @@ class OrderMapperTest {
                 // Remove all rows from all tables (first orders, then users to avoid foreign key constraint violation)
                 stmt.execute("DELETE FROM test.orders");
                 stmt.execute("DELETE FROM test.users");
+                stmt.execute("DELETE FROM test.shipping");
 
                 // Insert test data (first users, then orders to avoid foreign key constraint violation)
                 stmt.execute("INSERT INTO test.users (user_id, first_name, last_name, email, admin, address_id, password, phone_number) " +
@@ -53,6 +62,9 @@ class OrderMapperTest {
 
                 stmt.execute("INSERT INTO test.orders (order_id, price, user_id, comment, shipping_id, cp_length, cp_width , shed_length, shed_width, status_id, cp_roof) " +
                         "VALUES (1, 1000, 1, 'En god carport', 1, 480, 480, 200, 210, 1, 'Uden tag'), (2, 2000, 2, 'Test', 2, 600, 780, 0, 0, 2, 'Med plasttrapeztag')");
+
+                stmt.execute("INSERT INTO test.shipping (shipping_id, address_id, shipping_rate) VALUES (1, 1, 100), (2, 2, 200)");
+
                 // Set sequence to continue from the largest member_id
                 stmt.execute("SELECT setval('test.orders_order_id_seq', COALESCE((SELECT MAX(order_id) + 1 FROM test.orders), 1), false)");
                 stmt.execute("SELECT setval('test.users_user_id_seq', COALESCE((SELECT MAX(user_id) + 1 FROM test.users), 1), false)");
@@ -64,9 +76,19 @@ class OrderMapperTest {
     }
 
     @Test
-    void getAllOrders() {
+    void getAllOrders()
+    {
+        try
+        {
+            int expected = 2;
+            List<Order> actualOrders = OrderMapper.getAllOrders(connectionPool);
+            assertEquals(expected, actualOrders.size());
+        }
+        catch (DatabaseException e)
+        {
+            fail("Database fejl: " + e.getMessage());
+        }
     }
-
     @Test
     void getOrderByOrderId() {
     }
